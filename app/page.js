12abea89a2d3451e291c68f64aa72bf1b473ad60
Your Name <you@example.com>
2025-05-10 17:26:05 -0500

    updated hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1> Collet customer feedback to build better products </h1>
+      <div>
+        Create a feedback form to collect customer feedback and build better
+        products.
+      </div>
     </main>
   );
 }
